fdf6f8909eeab75809cbe50f70a25705ccd0bc83
bahalool <bahalool@gmail.com>
2018-11-06 13:28:20 +0200

    first commit

---
+++ consts/dir-names.js
@@ -0,0 +1,6 @@
+module.exports = {
+    DIR_NAMES: {
+        HKUBE: 'hkube',
+        HKUBE_RESULTS: 'hkube-results'
+    }
+};
---
+++ tests/test.js
@@ -0,0 +1,28 @@
+const { expect } = require('chai');
+const adapter = require('../lib/fs-adapter');
+
+describe('fs-adapter', () => {
+    before(async () => {
+        const options = {
+            Directory: process.env.FS_DIRECTORY || '/var/tmp/fs/storage/'
+        };
+        await adapter.init(options, null, true);
+    });
+    describe('put', () => {
+        it('put and get same value', async () => {
+            const link = await adapter.put({ jobId: Date.now(), taskId: 'task-1', data: 'test' });
+            const res = await adapter.get(link);
+            expect(res).to.equal('test');
+        });
+        it('put and get results same value', async () => {
+            const link = await adapter.putResults({ jobId: 'same-value-test', data: 'test' });
+            const res = await adapter.get(link);
+            expect(res).to.equal('test');
+        });
+    });
+    describe('jobPath', () => {
+        it('jobPath', async () => {
+            await adapter.jobPath({ jobId: 'same-value-test', taskId: 'task-1', data: 'test' });
+        });
+    });
+});
